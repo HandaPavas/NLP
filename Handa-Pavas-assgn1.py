a618c9c5f414f657f2977bc3a2b2dd575d91a30e
HandaPavas <pavas16040@iiitd.ac.in>
2017-08-16 17:12:40 +0530

    Sentences part modified

--- Handa-Pavas-assgn1.py
+++ Handa-Pavas-assgn1.py
@@ -3,48 +3,67 @@ import re
 import sys
 # open a file
 try:
-    filename = "/home/pavas/IIITD/NLP/Assignment 1/Development Set.txt"
+    filename = "/home/pavas/IIITD/NLP/Assignment 1/Test Set 1.txt"
     fileobject = open(filename, "r")
 except IOError:
     print 'Cannot open file %s for reading' % filename
     sys.exit(0)
 
 # Counting "words" in an article
-wordcount = 0
-lines = 0
-for line in fileobject.readlines():
-    if line != '\n':
-        for i in range(1, len(line)):
-            # extra spaces is taken care of
-            if line[i] in " " and line[i - 1] not in " ":
-                wordcount += 1
-        wordcount += 1
-# In-built function for splitting
-#    word_list = words.split()
-#    wordcount += len(word_list)
+# wordcount = 0
+# for line in fileobject.readlines():
+#    if line != '\n':
+#        for i in range(1, len(line)):
+#            # extra spaces is taken care of
+#            if line[i] in " " and line[i - 1] not in " ":
+#                wordcount += 1
+#        wordcount += 1
 
-print "Number of words in an article are: ", wordcount
+# print "Number of words in an article are: ", wordcount
+
+with open(filename, 'r') as f:
+    content = f.read()
+    content = re.sub(r'\n\n', ' ', content)
+    content = re.sub(r'\n', ' ', content)
+    word_list = content.split()
+print "Number of words in an article are: ", len(word_list)
 
 # Setting pointer to the start of the file
 fileobject.seek(0)
 
 # Counting "paragraphs" in an article
 paragraph_count = 0
-for line in fileobject.readlines():
-    if line == '\n':
+lines = fileobject.read().splitlines()
+for i in range(1, len(lines)):
+    line = lines[i]
+    prev_line = lines[i - 1]
+    if not line.split() and prev_line.split():
         paragraph_count += 1
+if lines[-1].split():
+    paragraph_count += 1
 print "Number of paragraphs in an article are: ", paragraph_count
-
-# Setting pointer to the start of the file
-fileobject.seek(0)
 
 # Counting "sentences" in an article
 sentence_count = 0
-for content in fileobject.readlines():
-    match_object = re.search("^(?!Mr|Ms|Mrs|Dr).*\.(\n|\s([A-Z])|(\"|\))\s)|^([0-9].+\.\s[0-9]).*|(!|\?)\s([A-Z])", content)
-    if match_object:
-        sentence_count += 1
-print "Number of sentences in an article are: ", sentence_count
+with open(filename, 'r') as f:
+    content = f.read()
+
+    # removes all 'blank lines' in the file
+    sentences = re.sub(r'\n\s*\n', '\n', content)
+
+    # removes all 'special characters except ? and ! and .' in the file
+    sentences = re.sub(r'"|;|\(|\)|:|,', '', sentences)
+
+    # classes any period after Mr/Mrs/Ms/Dr as ('not sentence boundaries')
+    sentences = re.sub(r'(?<!Mr)(?<!Mrs)(?<!Ms)(?<!Dr)\.\s([A-Z])', r'.\n\1', sentences)
+
+    # creates new line after exclamation mark
+    sentences = re.sub(r'!\s', '!\n', sentences)
+
+    # creates new line after question mark
+    sentences = re.sub(r'\?\s', '?\n', sentences)
+
+print "Number of sentences in an article are : ", len(sentences.split('\n'))
 
 # Close opened file
 fileobject.close()
