cfa598281bcb7a5b9c180bd0c651c9fda14848c9
HandaPavas <pavas16040@iiitd.ac.in>
2017-08-19 00:17:47 +0530

    Alignment Code Implemented

--- Handa-Pavas-assign2.py
+++ Handa-Pavas-assign2.py
@@ -16,7 +16,7 @@ def edit_distance(string_1, len_string1, string_2, len_string2):
             elif j == 0:
                 matrix[i][j] = i
 
-            # if element in both string are different, then there are three possiblites:
+            # if element in both string are different, then there are three possibilities:
             # 1. Insert
             # 2. Delete
             # 3. Substitute
@@ -28,16 +28,77 @@ def edit_distance(string_1, len_string1, string_2, len_string2):
             else:
                 matrix[i][j] = matrix[i-1][j-1]
     return matrix[i][j]
-
+print ("Enter strings in Uppercase !!!!")
 string1 = raw_input("Enter First String: ")
 string2 = raw_input("Enter Second String: ")
+print 'OUTPUT 1: '
 print "Minimum distance is: ", edit_distance(string1, len(string1), string2, len(string2))
+print '\n'
 
 
 # Function to print the alignment of the strings
 def alignment(string_1, len_string_1, string_2, len_string_2):
-    mod_string1 = string_1
+    i = len_string_1-1
+    j = len_string_2-1
+    while i >= 0 and j >= 0:
+        if string_1[i] == string_2[j]:
+            i -= 1
+            j -= 1
+        else:
+            row = i+1
+            column = j+1
+            values = [matrix[row][column-1],  # Insert
+                      matrix[row-1][column],  # Delete
+                      matrix[row-1][column-1]  # Substitute
+                      ]
+            index = values.index(min(values))
+            # Insert
+            if index == 0:
+                # if position greater than length of string
+                if i+1 >= len_string_1:
+                    string_1 = string_1 + '*'
+                else:
+                    string_1 = string_1[:i+1] + '*' + string_1[i+1:]
+                j -= 1
+            # Delete
+            elif index == 1:
+                # if position greater than length of string
+                if j+1 >= len_string_2:
+                    string_2 = string_2 + '*'
+                else:
+                    string_2 = string_2[:j+1] + '*' + string_2[j+1:]
+                i -= 1
+            # Substitute (Nothing to insert in string)
+            else:
+                i -= 1
+                j -= 1
 
+    if i == -1 and j != -1:
+        string_1 = ('*' * (j+1)) + string_1
+
+    elif j == -1 and i != -1:
+        string_2 = ('*' * (i + 1)) + string_2
+    print 'OUTPUT 2:'
+    print(string_1)
+    length = len(string_1)
+    print '|' * length
+    print(string_2)
+
+    i = 0
+    opertion = ""
+    while length != 0:
+        if string_1[i] != string_2[i]:
+            if string_1[i] != '*' and string_2[i] != '*':
+                opertion += 's'
+            elif string_1[i] == '*':
+                opertion += 'i'
+            else:
+                opertion += 'd'
+        else:
+            opertion += ' '
+        i += 1
+        length -= 1
+    print opertion
 
 
 alignment(string1, len(string1), string2, len(string2))
